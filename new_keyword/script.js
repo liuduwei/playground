7323e7d2a9357334d8322ff7575da5e2ffb676d0
liuduwei <17341730328@163.com>
2023-02-18 13:13:58 +0800

    myNew fn

--- new_keyword/script.js
+++ new_keyword/script.js
@@ -1,16 +1,27 @@
-const Person = function(name) {
+const Person = function(name, age) {
   this.name = name;
+  this.age = age;
 }
 
 Person.prototype.getName = function() {
   console.log(this.name);
 }
 
-const person1 = new Person('dfe');
+const person1 = new Person('dfe' ,18);
 console.log(person1);
 
 //1. create empty obj
+//2. empty obj.__proto__ = function.prototype
+//3. this = empty obj
+//4. return empty obj
 
-Function.prototype.myNew = function(...args) {
-  
+Function.prototype.myNew = function(name, age) { 
+  const obj = {};
+  obj.__proto__ = this.prototype;
+  this.apply(obj,arguments);
+  return obj;
 }
+const person2 = Person.myNew('dfedfe', 18);
+console.log(person2 instanceof Person);
+
+
